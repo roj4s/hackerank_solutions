8891c0003e9a7a329c15977fa0a5ecee643928ad
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2020-07-07 19:26:55 -0400

    Add sparse arrays
    Signed-off-by: Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>

---
+++ sparse_arrays.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the matchingStrings function below.
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+
+    vector<int> re (queries.size());
+
+    int rei = 0;
+    for(string q :  queries){
+        int ci = 0;
+        for(string s: strings){
+            if(s == q){
+                ci ++;
+            }
+        }
+        re.at(rei) = ci;
+        cout << ci << '\n';
+        rei ++;
+    }
+
+    return re;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int strings_count;
+    cin >> strings_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> strings(strings_count);
+
+    for (int i = 0; i < strings_count; i++) {
+        string strings_item;
+        getline(cin, strings_item);
+
+        strings[i] = strings_item;
+    }
+
+    int queries_count;
+    cin >> queries_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(strings, queries);
+
+    for (int i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
