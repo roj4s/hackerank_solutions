5f66e0d478ce863652a7cc50d447b8afc8cb9787
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2020-07-07 19:12:20 -0400

    Add left rotation
    Signed-off-by: Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>

---
+++ left_rotation.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+
+
+int main()
+{
+    string nd_temp;
+    getline(cin, nd_temp);
+
+    vector<string> nd = split_string(nd_temp);
+
+    int n = stoi(nd[0]);
+
+    int d = stoi(nd[1]);
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    int i = d;
+    int printed = 0;
+
+    while(printed != n){
+        if(i >= n){
+            i = 0;
+        }
+
+        cout <<  a_temp.at(i) << " ";
+        i ++;
+        printed ++;
+
+    }
+
+    return 0;
+
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+            return x == y and x == ' ';
+
+            });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+
+}
+
